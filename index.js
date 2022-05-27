caaf235d4a543423ffba7e5a2a26ca6e8f681bbb
ronnenn podolsky <ronnennpodolsky@ronnenns-MacBook-Pro.local>
2022-05-27 22:01:33 +0300

    saving leads and some more css

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const saveLead = () => alert("clicked");
 
-let inputBtn = document.querySelector("#input-btn")
+let myLeads = []
+
+const inputBtn = document.querySelector("#input-btn")
+const inputEl = document.querySelector("#input-el")
+const ulEl = document.querySelector("#ul-el")
+
+const renderLeads = value => ulEl.innerHTML += `<li>
+                                                    <a href='${value}' target='_blank'>${value}</a>
+                                                </li>`;
+
+
+const saveLead = () => {
+    if (inputEl.value === ""){
+        return
+    }
+    myLeads.push(inputEl.value);
+    // const li = document.createElement("li")
+    // li.textContent = inputEl.value;
+    // ulEl.append(li)  
+    renderLeads(inputEl.value)
+    inputEl.value = "";
+}
+
 inputBtn.addEventListener("click", saveLead)
+
